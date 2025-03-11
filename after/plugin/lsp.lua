c9be005c85bf9b32a9ebad07c85f4c03a3c02e7e
Some1and2 <04x0xx@gmail.com>
2025-03-11 12:23:01 -0300

    Added lspkind to `lsp.lua`

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,6 +4,7 @@ local lsp = require("lsp-zero").preset({})
 
 local eslint = require("eslint")
 local null_ls = require("null-ls")
+
 null_ls.setup()
 eslint.setup({
     bin = "eslint",
@@ -40,8 +41,8 @@ require'lspconfig'.lua_ls.setup {
     settings = {
         Lua = {
             diagnostics = {
-            	-- Get the language server to recognize the `vim` global
-            	globals = {'vim'},
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
             },
         },
     },
@@ -116,4 +117,27 @@ cmp.setup({
     }
 })
 
-require('lspkind').init()
+local lspkind = require('lspkind')
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = {
+        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        -- can also be a function to dynamically calculate max width such as
+        -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+        menu = 50, -- leading text (labelDetails)
+        abbr = 50, -- actual suggestion item
+      },
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        -- ...
+        return vim_item
+      end
+    })
+  }
+}
